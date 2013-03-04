d2d6dc5800fe5c70842c36400aa502345e40cef5
Sgarbo <sgarbo@localhost.localdomain>
2013-03-04 15:49:31 +0000

    Level 4

--- player.rb
+++ player.rb
@@ -1,8 +1,22 @@
 class Player
+  # método inicializar clase
+  def initialize()  
+    # atributos   
+    @health = 20
+    @attacking=false
+    @no_att=0
+  end
+  
   def play_turn(warrior)
+
     if warrior.feel.empty?
+      if @attacking
+	@no_att += 1
+	@attacking=false
+      end
+      
       #Only 7 of life is necessary to kill a sludge
-      if (warrior.health) >= 7
+      if ((warrior.health) >= 15) || (@health > warrior.health) || (@no_att == 3)
        warrior.walk!
       else
 	warrior.rest!
@@ -10,6 +24,9 @@ class Player
     else
      #if is not empty there is a monster attack
      warrior.attack!
+     @attacking=true
     end
+    
+    @health = warrior.health
   end
 end
