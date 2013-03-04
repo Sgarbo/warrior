c945f73a402a2f87c96664bc970f4c92fe1ab06d
Sgarbo <sgarbo@localhost.localdomain>
2013-03-04 16:13:57 +0000

    Level 5

--- player.rb
+++ player.rb
@@ -9,22 +9,30 @@ class Player
   
   def play_turn(warrior)
 
+    puts 'Last health ' + @health.to_s
+    puts 'Health ' + warrior.health.to_s
     if warrior.feel.empty?
       if @attacking
 	@no_att += 1
 	@attacking=false
       end
       
-      #Only 7 of life is necessary to kill a sludge
-      if ((warrior.health) >= 15) || (@health > warrior.health) || (@no_att == 3)
+      #More than 12 of life is necessary to kill a Sludge
+      #There are only 3 enemies
+      if (warrior.health > 12) || (@health > warrior.health) || (@no_att == 3)
        warrior.walk!
       else
 	warrior.rest!
       end
     else
-     #if is not empty there is a monster attack
-     warrior.attack!
-     @attacking=true
+      #if is not empty and there is a captive rescue
+      if warrior.feel.captive?
+	warrior.rescue!
+      else
+	#if is not empty and there is a monster attack
+	warrior.attack!
+	@attacking=true
+      end
     end
     
     @health = warrior.health
